56e1a1cb6242c52ee3f10c6690d12be6d28fbb3e
emotive44 <marko_streleshki_96@abv.bg>
2021-03-02 01:38:50 +0200

    react transition group

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,10 +1,11 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useRef } from 'react';
 import { useQuery } from '@apollo/client';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../shared/store/store';
 import { debouncedLogin, debouncedLogout } from '../../shared/store/actions/auth';
 import { GET_POSTS } from '../../shared/graphql/queries/posts';
 import classes from './Home.module.scss';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 
 
 interface HomeProps {
@@ -15,6 +16,8 @@ const Home:FC<HomeProps> = () => {
   // const { loading, error, data } = useQuery(GET_POSTS);
   const isAuth = useSelector((state: RootState) => state.auth.isAuth);
   const dispatch = useDispatch();
+  const nodeRef = useRef(null);
+  const nodeRef1 = useRef(null);
 
   const loginHandler = useCallback(
     () => dispatch(debouncedLogin()),
@@ -35,16 +38,51 @@ const Home:FC<HomeProps> = () => {
   // }
   
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
       <h1 className={classes.home}>Home</h1>
-      {/* <p>{JSON.stringify(data)}</p> */}
-      {isAuth 
-        ? <p>You are logged in</p>
-        : <p>You are not logged in</p>
-      }
+
+      {/* TOGGLE */}
+      <div style={{ overflow: 'hidden' }}>
+        <SwitchTransition mode="out-in">
+          <CSSTransition
+            timeout           = {400}
+            nodeRef           = {nodeRef}
+            key               = {isAuth ? 'auth' : 'not-auth'}
+            classNames={{
+              enter           : classes.enter,
+              enterActive     : classes['enter-active'],
+              enterDone       : classes['enter-done'],
+              exit            : classes.exit,
+              exitActive      : classes['exit-active'],
+              exitDone        : classes['exit-done'],
+            }}
+          >
+            <div ref={nodeRef}>
+              {!isAuth ? <p>You are NOT logged in</p> : <p>You are logged in</p> }
+            </div>
+          </CSSTransition>
+        </SwitchTransition>
+      </div>
 
       <button onClick={loginHandler}>Login</button>
       <button onClick={logoutHandler}>Logout</button>
+
+      {/* FADE */}
+      <div>
+        <CSSTransition
+          in={isAuth}
+          unmountOnExit
+          timeout             = {1000}
+          nodeRef             = {nodeRef1}
+          classNames={{
+            enter             : classes['enter-fade'],
+            enterActive       : classes['enter-active-fade'],
+            exitActive        : classes['exit-active-fade']
+          }}
+        >
+          <p ref={nodeRef1}>Fade Show/Hide</p>
+        </CSSTransition>
+      </div>
     </div>
   )
 }
